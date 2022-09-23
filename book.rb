433ec7dbe22ec2413a2ea960b685ef944d63bd7a
Joyrel Vaz <74455638+joyrel-vaz@users.noreply.github.com>
2022-09-23 20:36:30 +0530

    Update book.rb

--- book.rb
+++ book.rb
@@ -1,10 +1,10 @@
 class Book
   def initialize(name, author)
-    @@name = name
-    @@author = author
+    @name = name
+    @author = author
   end
   
   def set_page_bookmark(page_number)
-    @@page_bookmark = page_number
+    @page_bookmark = page_number
   end
 end
